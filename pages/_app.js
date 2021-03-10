9481a39cd0e7603a824689d20d0fd0f1cd9d2264
Ronny <ripunjoy.buddha@gmail.com>
2021-03-10 15:41:16 +0530

    Added head tag and next/image

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import React from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  
+  <React.Fragment>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
+    <Component {...pageProps} />
+  </React.Fragment>
+  )
 }
 
 export default MyApp
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const HomePage = () => {
+
+
+    const imgLoader = ({src, width, quality})=>{
+        return `${src}?w=${width}&q=${quality || 75}`
+    }
+
     return (
-        <div>
+        <React.Fragment>
+
+            <Head>
+                <title>Home Page</title>
+                <meta name="description" content="Find a lot of great events that allow you to evolve..."/>
+            </Head>
+
             <h1>Data Fetching with NextJS</h1>
+
+            <Image loader={imgLoader} src="https://images.pexels.com/photos/7036453/pexels-photo-7036453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="random" quality={10} width={500} height={500}/>
+            {/* <img src="https://images.pexels.com/photos/7036453/pexels-photo-7036453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="random" width={500} height={500}/> */}
 
             <ul>
                 <li><Link href="/product">Static site Generation</Link></li>
                 <li><Link href="/userProfile">Server side Rendering</Link></li>
             </ul>
-        </div>
+        </React.Fragment>
     )
 }
 
